c3472e577c1575352fac8031155d7b61f0e7af19
gabrielprimieri <gabriel_primieri@hotmail.com>
2024-10-23 11:46:43 -0300

    Projeto finalizado

---
+++ app.js
@@ -0,0 +1,58 @@
+// Seleciona o elemento h1 pelo ID
+const titulo = document.getElementById('titulo-principal');
+
+// Adiciona um evento de clique ao título
+titulo.addEventListener('click', () => {
+    // Redireciona para a página inicial
+    window.location.href = '';
+});
+
+function pesquisar(){
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if(campoPesquisa == ""){
+        section.innerHTML = "<p>Nada foi encontrado, digite alguma informação!</p>"
+       return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+// Inicializa uma string vazia para armazenar os resultados
+let resultados = "";
+let titulo = "";
+let descricao = "";
+let tags = "";
+
+
+// Itera sobre cada dado da lista de dados
+for (let dado of dados){
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+       // Cria um novo elemento HTML para cada resultado
+        resultados += `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+    </div>
+`;
+    }   
+}
+
+if (!resultados){
+    resultados ="<p>Nenhum atleta foi encontrado</p>"
+}
+
+// Atribui os resultados gerados á seção HTML
+section.innerHTML = resultados
+   
+}
---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Gabriel Araújo",
+        descricao: "Gabriel Araújo, mais conhecido como Gabrielzinho, é um dos maiores nomes da natação paralímpica brasileira. Com um talento inegável e uma determinação impressionante, o atleta mineiro já conquistou diversas medalhas em competições nacionais e internacionais, tornando-se um verdadeiro ídolo para os brasileiros.",
+        link: "https://pt.wikipedia.org/wiki/Gabrielzinho",
+        tags:"natação nataçao natacao Gabrielzinho gabrielzinho "
+        
+    },
+    {
+        titulo: "Lídia Cruz",
+        descricao: "Lídia Cruz é uma destacada nadadora paralímpica brasileira, conhecida por sua força e determinação. Com diversas medalhas em seu currículo, ela se tornou um exemplo para muitos atletas.",
+        link: "https://pt.wikipedia.org/wiki/L%C3%ADdia_Cruz",
+        tags:"natação nataçao natacao lidia Lidia lídia",
+    },
+    {
+        titulo: "Elizabeth Gomes",
+        descricao: "Elizabeth Gomes, mais conhecida como Beth Gomes é uma atleta paralímpica brasileira que se destaca no lançamento de disco. Sua precisão e força a tornaram uma das principais atletas da modalidade no país.",
+        link: "https://pt.wikipedia.org/wiki/Elizabeth_Gomes",
+        tags:"Beth beth Atletismo atletismo"
+    }
+];
